7b5929fb7282119195f8536b647a1049de26488f
Praveen Verma <praveenvermamail@gmail.com>
2022-07-30 14:37:10 +0530

    iphone11

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,7 +18,6 @@ public class App
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\vsver\\Downloads\\chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver();
@@ -23,6 +25,12 @@ public class App
 
         driver.navigate().to("https://www.amazon.in/");
         driver.manage().window().maximize();
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("I phone 11");
+        searchBox.submit();
+        driver.findElement(By.linkText("Apple iPhone 11 (64GB) - (Product) RED")).click();
+        Select quantity = new Select(driver.findElement(By.cssSelector("#select #quantity")));
+        quantity.selectByValue("2");
 
     }
 }
