dfabd50252c36e60eb711f62284ba611ee7078a9
Praveen Verma <praveenvermamail@gmail.com>
2022-08-01 19:24:54 +0530

    NoAlertException

--- src/main/java/org/example/NoErrorException.java
+++ src/main/java/org/example/NoErrorException.java
@@ -1,11 +1,11 @@
 package org.example;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class NoelementExceptions {
-    public static void main(String[] args) {
+public class NoErrorException {
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\vsver\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -17,12 +17,11 @@ public class NoelementExceptions {
             throw new RuntimeException(e);
         }
         try {
-            driver.findElement(By.name("Praveen")).click();
+            driver.switchTo().alert().accept();
         }
-        catch (Exception e) {
-            System.out.println("No such element found onPage");
-            System.out.println("Hello");
+        catch (NoAlertPresentException e) {
+           e.printStackTrace();
+
         }
-        System.out.println("Hello");
     }
 }
