22447f559abba42a82b926174048b320825d8b85
praveenverma99 <vermapraveen99@rediffmail.com>
2022-08-26 12:02:43 +0530

    sumofalldigit

---
+++ src/main/java/New/SumLastDigit.java
@@ -0,0 +1,22 @@
+package New;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class SumLastDigit {
+    public static void main(String[] args) {
+
+        System.out.print("Enter NO.: ");
+        Scanner scan =new Scanner(System.in);
+        int num = scan.nextInt();
+
+          int sum = 0;
+           while (num>0 ){
+
+                sum = sum + num%10;
+                num = num/10;
+
+        }
+        System.out.print( sum);
+    }
+}
