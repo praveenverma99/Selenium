652de6e8c2959ba0d64dbf76e685d3e47a76adf5
praveenverma99 <vermapraveen99@rediffmail.com>
2022-08-27 20:37:31 +0530

    Maxinarray

--- src/main/java/New/MissingNo/InArray.java
+++ src/main/java/New/MissingNo/InArray.java
@@ -1,9 +1,21 @@
 package New.MissingNo;
 
+import java.util.Scanner;
+
 public class InArray {
     public static void main(String[] args) {
 
-               int a[] = {10,20,30,100,50};
+           //    int a[] = {10,20,30,100,50};
+                  int a[] = new int[5];
+        Scanner scanner = new Scanner(System.in);
+                    for ( int i=0; i<a.length ; i++ )
+                    {
+                        System.out.println("Enter Value in array ");
+                        int n = scanner.nextInt();
+                        a[i] = n;
+                    }
+
+
                      int max = 0;
                        for (int i =0; i<a.length; i++)
                        {
