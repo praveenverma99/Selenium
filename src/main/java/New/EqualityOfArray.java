14d7be627c699ac21b18e3050f6304babf806ca0
praveenverma99 <vermapraveen99@rediffmail.com>
2022-08-27 18:44:08 +0530

    EqualArray

---
+++ src/main/java/New/EqualityOfArray.java
@@ -0,0 +1,44 @@
+package New;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class EqualityOfArray {
+    public static void main(String[] args) {
+
+                    int a[] = {1,2,3,4};
+                    int b[] = {1,2,3,5};
+
+              /*      boolean check = Arrays.equals(a,b);
+        //System.out.println(check);
+                 if( check==true)
+                 {
+                     System.out.println("Equal");
+                 }
+                else
+                 {
+                     System.out.println("Not Equal");
+                 }   */
+
+                boolean status = true;
+                if (a.length == b.length) {
+                    for (int i = 0; i < a.length; i++) {
+                        if (a[i] != b[i]) {
+                            status = false;
+                        } else {
+                            status = false;
+                        }
+                    }
+                }
+                if ( status == true)
+                          {
+                              System.out.println("Equal");
+                          }
+                          else
+                          {
+                              System.out.println("false");
+                          }
+                      }
+                }
+
+
--- src/main/java/New/OddEvenArrayCount.java
+++ src/main/java/New/OddEvenArrayCount.java
@@ -3,25 +3,22 @@ package New;
 public class OddEvenArrayCount {
     public static void main(String[] args) {
 
-              int a[] = {1,2,3,4,5,6,11};
-                   int even = 0;
-                   int odd = 0;
+        int a[] = {1, 2, 3, 4, 5, 6, 11};
+        int even = 0;
+        int odd = 0;
 
-                for( int i=0; i<=a.length-1; i++)
-                {
-                          int b = a[i];
-                           if(b%2==0) {
-                                  even++;
-                           }
-
-                            else
-                    {
-                          odd++;
-                    }
+        for (int i = 0; i <= a.length - 1; i++) {
+            int b = a[i];
+            if (b % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
 
 
-                }
+        }
         System.out.println("No. of even no. :" + even);
         System.out.println("No. of odd no. :" + odd);
+
+                }
     }
-}
