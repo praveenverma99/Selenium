1c9f5d4e1938e7c50ef7a4388b3a56c4dd19b09a
praveenverma99 <vermapraveen99@rediffmail.com>
2022-09-01 22:15:19 +0530

    RemoveSpaces

---
+++ src/main/java/New/MissingNo/RemoveWhiteSpaces.java
@@ -0,0 +1,12 @@
+package New.MissingNo;
+
+public class RemoveWhiteSpaces {
+    public static void main(String[] args) {
+
+        String str = "JAVA  programming  selenium    automation";
+
+               str= str.replaceAll("\\s", " ");
+
+                     System.out.println(str);
+      }
+}
