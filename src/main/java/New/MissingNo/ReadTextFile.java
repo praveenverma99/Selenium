f58381782b3ace19056ea5be217fd92fff80700a
praveenverma99 <vermapraveen99@rediffmail.com>
2022-09-02 16:56:26 +0530

    readtestfile

---
+++ src/main/java/New/MissingNo/ReadTextFile.java
@@ -0,0 +1,32 @@
+package New.MissingNo;
+
+import javax.annotation.processing.Filer;
+import java.io.*;
+import java.util.Scanner;
+
+public class ReadTextFile {
+    public static void main(String[] args) throws IOException {
+
+          /* FileReader fr = new FileReader("C:\\Users\\prave\\OneDrive\\Documents\\notepad..txt");
+           BufferedReader br = new BufferedReader(fr);
+
+             String str ;
+
+                while ((str=br.readLine())!=null)
+                {
+                    System.out.println(str);
+                }
+               br.close(); */
+
+          File file = new File("C:\\Users\\prave\\OneDrive\\Documents\\notepad..txt");
+          Scanner scan = new Scanner(file);
+
+                          while (scan.hasNextLine())
+                          {
+                              System.out.println(scan.nextLine());
+                          }
+
+                          scan.useDelimiter("\\z");
+                           System.out.println(scan.next());
+    }
+}
